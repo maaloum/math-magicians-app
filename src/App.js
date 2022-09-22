128c27604ab2d0cd9e90142e3dbb30d5482b9b77
Maaloum <50062179+maaloum@users.noreply.github.com>
2022-09-22 13:27:25 +0300

    Creation of full website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Quote from './components/Quote';
 
 function App() {
-  return <Calculator />;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/quote" element={<Quote />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -21,42 +21,58 @@ function Calculator() {
     setState((prevState) => ({ ...calculate(prevState, id) }));
   };
 
-  return (
-    <div className="claculator">
-      <div className="result">
-        {display}
-      </div>
-      <div className="panel">
-        <button onClick={handleChange} type="submit" id="allClear" className="btn"> AC </button>
-        <button onClick={handleChange} type="submit" id="switchSign" className="btn"> +/- </button>
-        <button onClick={handleChange} type="submit" id="percent" className="btn "> % </button>
-        <button onClick={handleChange} type="submit" id="division" className="operation"> ÷ </button>
-      </div>
-      <div className="panel">
-        <button onClick={handleChange} type="submit" id="btn7" className="btn"> 7 </button>
-        <button onClick={handleChange} type="submit" id="btn8" className="btn"> 8 </button>
-        <button onClick={handleChange} type="submit" id="btn9" className="btn"> 9 </button>
-        <button onClick={handleChange} type="submit" id="multiplication" className="operation"> x </button>
-      </div>
-      <div className="panel">
-        <button onClick={handleChange} type="submit" id="btn4" className="btn"> 4 </button>
-        <button onClick={handleChange} type="submit" id="btn5" className="btn"> 5 </button>
-        <button onClick={handleChange} type="submit" id="btn6" className="btn"> 6 </button>
-        <button onClick={handleChange} type="submit" id="sub" className="operation"> - </button>
-      </div>
-      <div className="panel">
-        <button onClick={handleChange} type="submit" id="btn1" className="btn"> 1 </button>
-        <button onClick={handleChange} type="submit" id="btn2" className="btn"> 2 </button>
-        <button onClick={handleChange} type="submit" id="btn3" className="btn"> 3 </button>
-        <button onClick={handleChange} type="submit" id="addition" className="operation"> + </button>
-      </div>
-      <div className="panel">
-        <button onClick={handleChange} type="submit" className="zero"> 0 </button>
-        <button onClick={handleChange} type="submit" className="btn"> . </button>
-        <button onClick={handleChange} type="submit" className="operation"> = </button>
-      </div>
+  const container = {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'flex-start',
+    gap: '10px',
+    marginTop: '50px',
+  };
+  const p = {
+    color: 'black',
+    fontSize: '20px',
+  };
 
+  return (
+    <div style={container}>
+      <p style={p}>Let&apos;s do some math!</p>
+      <div className="claculator">
+        <div className="result">
+          {display}
+        </div>
+        <div className="panel">
+          <button onClick={handleChange} type="submit" id="allClear" className="btn"> AC </button>
+          <button onClick={handleChange} type="submit" id="switchSign" className="btn"> +/- </button>
+          <button onClick={handleChange} type="submit" id="percent" className="btn "> % </button>
+          <button onClick={handleChange} type="submit" id="division" className="operation"> ÷ </button>
+        </div>
+        <div className="panel">
+          <button onClick={handleChange} type="submit" id="btn7" className="btn"> 7 </button>
+          <button onClick={handleChange} type="submit" id="btn8" className="btn"> 8 </button>
+          <button onClick={handleChange} type="submit" id="btn9" className="btn"> 9 </button>
+          <button onClick={handleChange} type="submit" id="multiplication" className="operation"> x </button>
+        </div>
+        <div className="panel">
+          <button onClick={handleChange} type="submit" id="btn4" className="btn"> 4 </button>
+          <button onClick={handleChange} type="submit" id="btn5" className="btn"> 5 </button>
+          <button onClick={handleChange} type="submit" id="btn6" className="btn"> 6 </button>
+          <button onClick={handleChange} type="submit" id="sub" className="operation"> - </button>
+        </div>
+        <div className="panel">
+          <button onClick={handleChange} type="submit" id="btn1" className="btn"> 1 </button>
+          <button onClick={handleChange} type="submit" id="btn2" className="btn"> 2 </button>
+          <button onClick={handleChange} type="submit" id="btn3" className="btn"> 3 </button>
+          <button onClick={handleChange} type="submit" id="addition" className="operation"> + </button>
+        </div>
+        <div className="panel">
+          <button onClick={handleChange} type="submit" className="zero"> 0 </button>
+          <button onClick={handleChange} type="submit" className="btn"> . </button>
+          <button onClick={handleChange} type="submit" className="operation"> = </button>
+        </div>
+
+      </div>
     </div>
+
   );
 }
 
